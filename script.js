6e7f81b5b559b33247b27fbb64dea26c542d89f4
MaryJenniferY <67652201+MaryJenniferY@users.noreply.github.com>
2020-08-04 21:17:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function vmon() {
+  document.getElementById("month").style.display = "contents";
+  document.getElementById("year").style.display = "none";
+}
+
+function vyear() {
+  document.getElementById("year").style.display = "contents";
+  document.getElementById("month").style.display = "none";
+}
